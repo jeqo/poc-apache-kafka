b97e10fbeca0715042afb363adf7a53cf7969ff0
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2021-05-10 22:23:18 +0100

    testing poc

---
+++ streams/poc-streams-testing/src/test/java/poc/StreamsTestingAppTest.java
@@ -0,0 +1,48 @@
+package poc;
+
+
+import static poc.StreamsTestingApp.buildSimpleFromTo;
+import static poc.StreamsTestingApp.loadConfig;
+
+import java.io.IOException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.TopologyTestDriver;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+class StreamsTestingAppTest {
+
+  @Test
+  void testApp() throws IOException {
+    testBasicFilter();
+  }
+
+  void testBasicFilter() throws IOException {
+    // Given
+    final var driver = new TopologyTestDriver(buildSimpleFromTo(), loadConfig());
+    // - with topics
+    var inputTopic = driver.createInputTopic(
+        "input",
+        new StringSerializer(),
+        new StringSerializer()
+    );
+    var outputTopic = driver.createOutputTopic(
+        "output",
+        new StringDeserializer(),
+        new StringDeserializer()
+    );
+
+    // When
+    inputTopic.pipeInput("k1", "v1");
+    var kv1 = outputTopic.readKeyValue();
+
+    // Then
+    Assertions.assertThat(kv1)
+        .extracting("key", "value")
+        .contains("k1", "v1");
+  }
+}
