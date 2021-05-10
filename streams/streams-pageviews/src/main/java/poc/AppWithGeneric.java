b99361a493409a2d631afa99e2756fb2612d2cae
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2021-05-10 20:24:15 +0100

    reorganize pocs

---
+++ streams/streams-pageviews/src/main/java/poc/AppWithGeneric.java
@@ -0,0 +1,13 @@
+package poc;
+
+import java.util.Properties;
+
+public class AppWithGeneric {
+
+  public static void main(String[] args) {
+    final var configs = new Properties();
+    configs.put("bootstrap.servers", "localhost:9092");
+
+
+  }
+}
---
+++ streams/streams-pageviews/src/main/java/poc/ProducerWithGeneric.java
@@ -0,0 +1,38 @@
+package poc;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class ProducerWithGeneric {
+
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+    final var configs = new Properties();
+    configs.put("bootstrap.servers", "localhost:9092");
+    configs.put("key.serializer", "io.confluent.kafka.streams.serdes.avro.GenericAvroSerializer");
+    configs.put("value.serializer", "io.confluent.kafka.streams.serdes.avro.GenericAvroSerializer");
+    configs.put("schema.registry.url", "http://localhost:8081");
+
+    final var producer = new KafkaProducer<GenericRecord, GenericRecord>(configs);
+
+    final var keySchema = SchemaBuilder.record("Key").namespace("poc.avro")
+        .fields()
+        .name("val1").type().stringType().noDefault()
+        .endRecord();
+    final var key = new GenericData.Record(keySchema);
+    key.put("val1", "1");
+    final var valueSchema = SchemaBuilder.record("Value").namespace("poc.avro")
+        .fields()
+        .name("val1").type().stringType().noDefault()
+        .endRecord();
+    final var value = new GenericData.Record(valueSchema);
+    value.put("val1", "1");
+
+    final var record = new ProducerRecord<GenericRecord, GenericRecord>("t1", key, value);
+    producer.send(record).get();
+  }
+}
