bc6db58d8fd2831bc48a3babf0ddc2f75dde0bc6
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2022-03-02 09:00:32 +0000

    fix(cli:topic-list): close clients

--- cli/topic-list/src/main/java/kafka/cli/topics/TopicListCli.java
+++ cli/topic-list/src/main/java/kafka/cli/topics/TopicListCli.java
@@ -3,7 +3,6 @@ package kafka.cli.topics;
 import static java.lang.System.out;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.stream.Collectors;
 import kafka.cli.topics.TopicListCli.VersionProviderWithConfigProvider;
 import org.apache.kafka.clients.admin.AdminClient;
@@ -24,7 +23,7 @@ import java.util.concurrent.ExecutionException;
 import picocli.CommandLine.IVersionProvider;
 
 @CommandLine.Command(
-    name = "kafka-list-topics",
+    name = "ktopiclist",
     versionProvider = VersionProviderWithConfigProvider.class,
     mixinStandardHelpOptions = true
 )
@@ -53,59 +52,62 @@ public class TopicListCli implements Callable<Integer> {
         var config = new Config(topics, prefix);
         var clientConfig = new Properties();
         clientConfig.load(Files.newInputStream(configPath));
-        var adminClient = AdminClient.create(clientConfig);
-        List<String> list = listOfTopics(config, adminClient);
-        out.println("List of topics:");
-        out.println(list);
-        out.println();
-        var described = adminClient.describeTopics(list).allTopicNames().get();
-        var configs = adminClient.describeConfigs(
-                list.stream().map(t -> new ConfigResource(ConfigResource.Type.TOPIC, t)).toList()).all()
-            .get();
-        var tpsByTopic = new LinkedHashMap<String, List<TopicPartition>>();
-        var startOffsetRequest = new LinkedHashMap<TopicPartition, OffsetSpec>();
-        var endOffsetRequest = new LinkedHashMap<TopicPartition, OffsetSpec>();
-        for (String topic : list) {
-            var description = described.get(topic);
-            var tps = description.partitions().stream()
-                .map(tpi -> new TopicPartition(topic, tpi.partition()))
-                .sorted(Comparator.comparingInt(TopicPartition::partition))
-                .toList();
-            tps.forEach(topicPartition -> {
-                startOffsetRequest.put(topicPartition, OffsetSpec.earliest());
-                endOffsetRequest.put(topicPartition, OffsetSpec.latest());
-            });
-            tpsByTopic.put(topic, tps);
-        }
-        var startOffsets = adminClient.listOffsets(startOffsetRequest).all().get();
-        var endOffsets = adminClient.listOffsets(endOffsetRequest).all().get();
-        var numberFormat = NumberFormat.getInstance();
-        final var length = String.valueOf(Long.MAX_VALUE).length();
-        for (String topic : list) {
-            var description = described.get(topic);
-            var tps = description.partitions().stream()
-                .collect(Collectors.toMap(TopicPartitionInfo::partition, tpi -> tpi));
-            out.printf("Topic: %s (UID: %s)%n", topic, description.topicId());
-            out.println(
-                " Configs: " + configs.get(new ConfigResource(ConfigResource.Type.TOPIC, topic)));
-            for (var tp : tpsByTopic.get(topic)) {
-                var tpi = tps.get(tp.partition());
-                out.println(" Partitions:");
-                out.printf("  %s: [Leader: %s] [ISR: %s] %n",
-                    tp.partition(),
-                    tpi.leader().id(),
-                    tpi.isr().stream()
-                        .map(node -> node.hasRack() ?
-                            node.id() + " (" + node.host() + "@rack:" + node.rack() + ")" :
-                            node.id() + " (" + node.host() + ")")
-                        .toList());
-                out.printf("   offsets: [ %s - %s ]%n",
-                    String.format("%1$" + length + "s",
-                        numberFormat.format(startOffsets.get(tp).offset())).replace(' ', '_'),
-                    String.format("%1$" + length + "s",
-                        numberFormat.format(endOffsets.get(tp).offset())).replace(' ', '_'));
-            }
+        try (var adminClient = AdminClient.create(clientConfig)) {
+            List<String> list = listOfTopics(config, adminClient);
+            out.println("List of topics:");
+            out.println(list);
             out.println();
+            var described = adminClient.describeTopics(list).allTopicNames().get();
+            var configs = adminClient.describeConfigs(
+                    list.stream().map(t -> new ConfigResource(ConfigResource.Type.TOPIC, t)).toList())
+                .all()
+                .get();
+            var tpsByTopic = new LinkedHashMap<String, List<TopicPartition>>();
+            var startOffsetRequest = new LinkedHashMap<TopicPartition, OffsetSpec>();
+            var endOffsetRequest = new LinkedHashMap<TopicPartition, OffsetSpec>();
+            for (String topic : list) {
+                var description = described.get(topic);
+                var tps = description.partitions().stream()
+                    .map(tpi -> new TopicPartition(topic, tpi.partition()))
+                    .sorted(Comparator.comparingInt(TopicPartition::partition))
+                    .toList();
+                tps.forEach(topicPartition -> {
+                    startOffsetRequest.put(topicPartition, OffsetSpec.earliest());
+                    endOffsetRequest.put(topicPartition, OffsetSpec.latest());
+                });
+                tpsByTopic.put(topic, tps);
+            }
+            var startOffsets = adminClient.listOffsets(startOffsetRequest).all().get();
+            var endOffsets = adminClient.listOffsets(endOffsetRequest).all().get();
+            var numberFormat = NumberFormat.getInstance();
+            final var length = String.valueOf(Long.MAX_VALUE).length();
+            for (String topic : list) {
+                var description = described.get(topic);
+                var tps = description.partitions().stream()
+                    .collect(Collectors.toMap(TopicPartitionInfo::partition, tpi -> tpi));
+                out.printf("Topic: %s (UID: %s)%n", topic, description.topicId());
+                out.println(
+                    " Configs: " + configs.get(
+                        new ConfigResource(ConfigResource.Type.TOPIC, topic)));
+                for (var tp : tpsByTopic.get(topic)) {
+                    var tpi = tps.get(tp.partition());
+                    out.println(" Partitions:");
+                    out.printf("  %s: [Leader: %s] [ISR: %s] %n",
+                        tp.partition(),
+                        tpi.leader().id(),
+                        tpi.isr().stream()
+                            .map(node -> node.hasRack() ?
+                                node.id() + " (" + node.host() + "@rack:" + node.rack() + ")" :
+                                node.id() + " (" + node.host() + ")")
+                            .toList());
+                    out.printf("   offsets: [ %s - %s ]%n",
+                        String.format("%1$" + length + "s",
+                            numberFormat.format(startOffsets.get(tp).offset())).replace(' ', '_'),
+                        String.format("%1$" + length + "s",
+                            numberFormat.format(endOffsets.get(tp).offset())).replace(' ', '_'));
+                }
+                out.println();
+            }
         }
         return 0;
     }
@@ -131,11 +133,11 @@ public class TopicListCli implements Callable<Integer> {
 
         @Override
         public String[] getVersion() throws IOException {
-            var url = VersionProviderWithConfigProvider.class.getClassLoader().getResource("cli.properties");
+            final var url = VersionProviderWithConfigProvider.class.getClassLoader().getResource("cli.properties");
             if (url == null) {
                 return new String[]{"No cli.properties file found in the classpath."};
             }
-            Properties properties = new Properties();
+            final var properties = new Properties();
             properties.load(url.openStream());
             return new String[]{
                 properties.getProperty("appName") + " version " + properties.getProperty(
