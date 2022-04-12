56fd0689019e921c506da402aab5c6c2c3767d43
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2022-04-12 10:20:04 +0100

    feat(cli:quotas): add query command

--- cli/quotas/src/main/java/kafka/cli/quotas/Cli.java
+++ cli/quotas/src/main/java/kafka/cli/quotas/Cli.java
@@ -1,14 +1,21 @@
 package kafka.cli.quotas;
 
 import static java.lang.System.err;
+import static java.lang.System.out;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 import kafka.cli.quotas.Cli.CreateCommand;
+import kafka.cli.quotas.Cli.QueryCommand;
 import kafka.cli.quotas.Quotas.ConnectionCreationRate;
 import kafka.cli.quotas.Quotas.Constraint;
 import kafka.cli.quotas.Quotas.KafkaClient;
@@ -23,11 +30,14 @@ import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-@Command(name = "kfk-quotas", subcommands = {CreateCommand.class})
+@Command(
+    name = "kfk-quotas",
+    subcommands = {
+        QueryCommand.class,
+        CreateCommand.class
+    }
+)
 public class Cli implements Callable<Integer> {
-
-    @ArgGroup(multiplicity = "1")
-    PropertiesOption propertiesOption;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new Cli()).execute(args);
@@ -35,14 +45,77 @@ public class Cli implements Callable<Integer> {
     }
 
     @Override
-    public Integer call() throws Exception {
-        final var props = propertiesOption.load();
-        final var kafkaAdmin = AdminClient.create(props);
-        final var quotaManager = new QuotaManager(kafkaAdmin);
-
-        final var quotas = quotaManager.all();
-        System.out.println(quotas.toJson());
+    public Integer call() {
+        CommandLine.usage(this, out);
         return 0;
+    }
+
+    @Command(name = "query")
+    static class QueryCommand implements Callable<Integer> {
+
+        @ArgGroup(multiplicity = "1")
+        PropertiesOption propertiesOption;
+
+        @Option(names = {"--all-users"})
+        boolean allUsers;
+        @Option(names = {"--all-clients"})
+        boolean allClients;
+        @Option(names = {"--all-ips"})
+        boolean allIps;
+
+        @Option(names = {"--user-clients"})
+        Map<String, String> userClients = new HashMap<>();
+
+        @Option(names = {"--user"})
+        List<String> users = new ArrayList<>();
+        @Option(names = {"--user-default"})
+        boolean userDefault;
+
+        @Option(names = {"--client"})
+        List<String> clientIds = new ArrayList<>();
+        @Option(names = {"--client-default"})
+        boolean clientIdDefault;
+
+        @Option(names = {"--ip"})
+        List<String> ips = new ArrayList<>();
+        @Option(names = {"--ip-default"})
+        boolean ipDefault;
+
+        @Override
+        public Integer call() throws Exception {
+            final var props = propertiesOption.load();
+            final var kafkaAdmin = AdminClient.create(props);
+            final var quotaManager = new QuotaManager(kafkaAdmin);
+
+            if (allUsers) {
+                final var quotas = quotaManager.allByUsers();
+                System.out.println(quotas.toJson());
+            } else if (allClients) {
+                final var quotas = quotaManager.allByClients();
+                System.out.println(quotas.toJson());
+            } else if (allIps) {
+                final var quotas = quotaManager.allByIps();
+                System.out.println(quotas.toJson());
+            } else if (!userClients.isEmpty()) {
+                final var quotas = quotaManager.byUsers(userClients.keySet().stream()
+                    .collect(
+                        Collectors.toMap(k -> k, k -> List.of(userClients.get(k).split(",")))));
+                System.out.println(quotas.toJson());
+            } else if (userDefault || !users.isEmpty()) {
+                final var quotas = quotaManager.byUsers(users, userDefault);
+                System.out.println(quotas.toJson());
+            } else if (clientIdDefault || !clientIds.isEmpty()) {
+                final var quotas = quotaManager.byClients(clientIds, clientIdDefault);
+                System.out.println(quotas.toJson());
+            } else if (ipDefault || !ips.isEmpty()) {
+                final var quotas = quotaManager.byIps(ips, ipDefault);
+                System.out.println(quotas.toJson());
+            } else {
+                final var quotas = quotaManager.all();
+                System.out.println(quotas.toJson());
+            }
+            return 0;
+        }
     }
 
     @Command(name = "create")
@@ -55,18 +128,20 @@ public class Cli implements Callable<Integer> {
         boolean userDefault;
         @Option(names = {"--user"}, description = "Application's User Principal")
         Optional<String> user;
-        @Option(names = {"--client-id-default"}, description = "Default to all client IDs")
+
+        @Option(names = {"--client-default"}, description = "Default to all client IDs")
         boolean clientIdDefault;
-        @Option(names = {"--client-id"}, description = "Application's Client ID")
+        @Option(names = {"--client"}, description = "Application's Client ID")
         Optional<String> clientId;
+
         @Option(names = {"--ip-default"}, description = "Default to all IPs")
         boolean ipDefault;
         @Option(names = {"--ip"}, description = "Application's IP")
         Optional<String> ip;
 
-        @Option(names = {"--write"}, description = "Write bandwidth")
+        @Option(names = {"--produce-rate"}, description = "Write bandwidth")
         Optional<Long> writeBandwidth;
-        @Option(names = {"--read"}, description = "Read bandwidth")
+        @Option(names = {"--fetch-rate"}, description = "Read bandwidth")
         Optional<Long> readBandwidth;
         @Option(names = {"--request-rate"}, description = "Request rate")
         Optional<Long> requestRate;
@@ -95,7 +170,6 @@ public class Cli implements Callable<Integer> {
             return 0;
         }
     }
-
 
     static class PropertiesOption {
 
--- cli/quotas/src/main/java/kafka/cli/quotas/QuotaManager.java
+++ cli/quotas/src/main/java/kafka/cli/quotas/QuotaManager.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 import kafka.cli.quotas.Quotas.Quota;
 import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.common.quota.ClientQuotaAlteration;
 import org.apache.kafka.common.quota.ClientQuotaEntity;
 import org.apache.kafka.common.quota.ClientQuotaFilter;
 import org.apache.kafka.common.quota.ClientQuotaFilterComponent;
@@ -40,39 +40,72 @@ public class QuotaManager {
         }
     }
 
-    public Quotas allByUser() {
+    public Quotas allByUsers() {
         final var conditions = List.of(ClientQuotaFilterComponent.ofEntityType(ClientQuotaEntity.USER));
         final var filter = ClientQuotaFilter.contains(conditions);
         return query(filter);
     }
 
-    public Quotas allByClient() {
+    public Quotas allByClients() {
         final var conditions = List.of(ClientQuotaFilterComponent.ofEntityType(ClientQuotaEntity.CLIENT_ID));
         final var filter = ClientQuotaFilter.contains(conditions);
         return query(filter);
     }
 
-    public Quotas byUsers(List<String> users) {
-        final var filter = ClientQuotaFilter.contains(
-            users.stream()
-                .map(u -> ClientQuotaFilterComponent.ofEntity(ClientQuotaEntity.USER, u))
-                .toList());
+    public Quotas allByIps() {
+        final var conditions = List.of(ClientQuotaFilterComponent.ofEntityType(ClientQuotaEntity.IP));
+        final var filter = ClientQuotaFilter.contains(conditions);
+        return query(filter);
+    }
+
+    public Quotas byUsers(List<String> users, boolean userDefault) {
+        final var components = users.stream()
+            .map(id -> ClientQuotaFilterComponent.ofEntity(ClientQuotaEntity.USER, id))
+            .collect(Collectors.toList());
+        if (userDefault) {
+            components.add(ClientQuotaFilterComponent.ofDefaultEntity(ClientQuotaEntity.USER));
+        }
+        final var filter = ClientQuotaFilter.containsOnly(components);
         return query(filter);
     }
 
     public Quotas byUsers(Map<String, List<String>> users) {
-        final var filter = ClientQuotaFilter.contains(
-            users.keySet().stream()
-                .map(u -> ClientQuotaFilterComponent.ofEntity(ClientQuotaEntity.USER, u))
-                .toList());
+//        final var components = users.keySet().stream()
+//            .map(u -> ClientQuotaFilterComponent.ofEntity(ClientQuotaEntity.USER, u))
+//            .toList();
+        final var all = new Quotas(new ArrayList<>());
+        final var components = new ArrayList<ClientQuotaFilterComponent>();
+        for (final var user : users.keySet()) {
+            components.add(ClientQuotaFilterComponent.ofEntity(ClientQuotaEntity.USER, user));
+            for (final var client : users.get(user)) {
+                components.add(ClientQuotaFilterComponent.ofEntity(ClientQuotaEntity.CLIENT_ID, client));
+            }
+            final var filter = ClientQuotaFilter.containsOnly(components);
+            final var quotas = query(filter);
+            all.append(quotas);
+        }
+        return all;
+    }
+
+    public Quotas byClients(List<String> clientIds, boolean clientIdDefault) {
+        final var components = clientIds.stream()
+            .map(id -> ClientQuotaFilterComponent.ofEntity(ClientQuotaEntity.CLIENT_ID, id))
+            .collect(Collectors.toList());
+        if (clientIdDefault) {
+            components.add(ClientQuotaFilterComponent.ofDefaultEntity(ClientQuotaEntity.CLIENT_ID));
+        }
+        final var filter = ClientQuotaFilter.containsOnly(components);
         return query(filter);
     }
 
-    public Quotas byClients(List<String> clientIds) {
-        final var filter = ClientQuotaFilter.contains(
-            clientIds.stream()
-                .map(id -> ClientQuotaFilterComponent.ofEntity(ClientQuotaEntity.CLIENT_ID, id))
-                .toList());
+    public Quotas byIps(List<String> ips, boolean ipDefault) {
+        final var components = ips.stream()
+            .map(id -> ClientQuotaFilterComponent.ofEntity(ClientQuotaEntity.IP, id))
+            .collect(Collectors.toList());
+        if (ipDefault) {
+            components.add(ClientQuotaFilterComponent.ofDefaultEntity(ClientQuotaEntity.IP));
+        }
+        final var filter = ClientQuotaFilter.containsOnly(components);
         return query(filter);
     }
 
