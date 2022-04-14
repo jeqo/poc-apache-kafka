e86a3b153f102059e6ea40fc41e262871149d6c7
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2022-04-14 18:24:23 +0100

    feat(client:producer-progress-control): add interceptor

--- clients/producer-progress-control/src/main/java/kafka/producer/ProgressControlInterceptor.java
+++ clients/producer-progress-control/src/main/java/kafka/producer/ProgressControlInterceptor.java
@@ -1,29 +1,61 @@
 package kafka.producer;
 
+import java.io.IOException;
 import java.util.Map;
+import java.util.Properties;
+import kafka.producer.ProgressController.Config;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerInterceptor;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
 
 public class ProgressControlInterceptor<K, V> implements ProducerInterceptor<K, V> {
 
-  @Override
-  public ProducerRecord<K, V> onSend(ProducerRecord<K, V> record) {
-    return null;
-  }
-
-  @Override
-  public void onAcknowledgement(RecordMetadata metadata, Exception exception) {
-
-  }
-
-  @Override
-  public void close() {
-
-  }
+  ProgressController<K, V> progressController;
+  volatile boolean closeCalled;
 
   @Override
   public void configure(Map<String, ?> configs) {
+    if (progressController == null) {
+      synchronized (this) {
+        final var producerConfig = new Properties(configs.size());
+        producerConfig.putAll(configs);
+        if (!producerConfig.containsKey("key.serializer"))
+          producerConfig.put("key.serializer", ByteArraySerializer.class);
+        if (!producerConfig.containsKey("value.serializer"))
+          producerConfig.put("value.serializer", ByteArraySerializer.class);
+        final var internalProducer = new KafkaProducer<K, V>(producerConfig);
+        final var config = Config.newBuilder();
+        this.progressController = new ProgressController<>(internalProducer, config.build());
+        Thread controlThread = new Thread(this.progressController);
+        controlThread.start();
+      }
+    }
+  }
 
+  @Override
+  public ProducerRecord<K, V> onSend(ProducerRecord<K, V> record) {
+    if (closeCalled) return record;
+    progressController.addTopicPartition(record);
+    return record;
+  }
+
+  @Override
+  public void onAcknowledgement(RecordMetadata metadata, Exception exception) {}
+
+  @Override
+  public synchronized void close() {
+    if (closeCalled) {
+      return;
+    }
+    if (progressController != null) {
+      try {
+        progressController.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    closeCalled = true;
   }
 }
--- clients/producer-progress-control/src/test/java/kafka/producer/ProgressControllerTest.java
+++ clients/producer-progress-control/src/test/java/kafka/producer/ProgressControllerTest.java
@@ -1,22 +1,33 @@
 package kafka.producer;
 
-import java.io.IOException;
-import kafka.producer.ProgressController.Config;
-import kafka.producer.ProgressController.Control;
-import org.apache.kafka.common.TopicPartition;
-import org.junit.jupiter.api.Test;
-
-import java.time.Duration;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+import kafka.producer.ProgressController.Config;
+import kafka.producer.ProgressController.Control;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.jupiter.api.Test;
+
 class ProgressControllerTest {
+  final Producer<String, String> producer;
+
+  ProgressControllerTest() {
+    final var properties = new Properties();
+    properties.put("bootstrap.servers", "localhost:19092");
+    producer = new KafkaProducer<>(properties, new StringSerializer(), new StringSerializer());
+  }
 
   @Test
-  void test() {
+  void shouldEvalFalse_whenDiffIsLessThanStartPeriod() {
     final var config = Config.newBuilder().withStart(Duration.ofSeconds(1)).build();
-    try(final var controller = new ProgressController(config)) {
+    try (final var controller = new ProgressController<>(producer, config)) {
       final var tp = new TopicPartition("t1", 0);
       final var eval = controller.eval(tp, Control.create(0), 10);
       assertFalse(eval);
@@ -26,13 +37,67 @@ class ProgressControllerTest {
   }
 
   @Test
-  void test1() {
+  void shouldEvalTrue_whenDiffIsHigherThanStartPeriod() {
     final var config = Config.newBuilder().withStart(Duration.ofSeconds(1)).build();
-    try(final var controller = new ProgressController(config)) {
+    try (final var controller = new ProgressController<>(producer, config)) {
       final var tp = new TopicPartition("t1", 0);
       controller.addTopicPartition(tp, 0);
-      final var eval = controller.eval(tp, Control.create(0), Duration.ofSeconds(2).toMillis());
+      final var current = Duration.ofSeconds(2).toMillis();
+      final var eval = controller.eval(tp, Control.create(0), current);
       assertTrue(eval);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  void shouldRemovePartition_whenEvalTrueAndNoBackoff() {
+    final var config = Config.newBuilder().withStart(Duration.ofSeconds(1)).build();
+    try (final var controller = new ProgressController<>(producer, config)) {
+      final var tp = new TopicPartition("t1", 0);
+      controller.addTopicPartition(tp, 0);
+      final var current = Duration.ofSeconds(2).toMillis();
+      final var eval = controller.eval(tp, Control.create(0), current);
+      assertTrue(eval);
+      assertTrue(controller.progress.isEmpty());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  void shouldIncreaseIteration_whenEvalTrueAndNoBackoff() {
+    final var config =
+        Config.newBuilder()
+            .withStart(Duration.ofSeconds(1))
+            .withEnd(Duration.ofSeconds(10), Duration.ofSeconds(1), false)
+            .build();
+    try (final var controller = new ProgressController<>(producer, config)) {
+      final var tp = new TopicPartition("t1", 0);
+      controller.addTopicPartition(tp, 0);
+      final var current = Duration.ofSeconds(2).toMillis();
+      final var eval = controller.eval(tp, Control.create(0), current);
+      assertTrue(eval);
+      assertFalse(controller.progress.isEmpty());
+      assertEquals(1, controller.progress.get(tp).iteration());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  void shouldEvalFalse_when() {
+    final var config =
+        Config.newBuilder()
+            .withStart(Duration.ofSeconds(1))
+            .withEnd(Duration.ofSeconds(10), Duration.ofSeconds(2), false)
+            .build();
+    try (final var controller = new ProgressController<>(producer, config)) {
+      final var tp = new TopicPartition("t1", 0);
+      controller.addTopicPartition(tp, 0);
+      final var current = Duration.ofSeconds(1).toMillis();
+      final var eval = controller.eval(tp, new Control(0, current, 1), current);
+      assertFalse(eval);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
