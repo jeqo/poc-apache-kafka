5e6c00098d6c6460871c9c3015880d018c927cbf
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2022-05-16 16:44:34 +0100

    refactor: example location

--- clients/producer-progress-control/src/test/java/kafka/producer/examples/ProgressControlInterceptorExample.java
+++ clients/producer-progress-control/src/test/java/kafka/producer/examples/ProgressControlInterceptorExample.java
@@ -1,5 +1,6 @@
-package kafka.producer;
+package kafka.producer.examples;
 
+import kafka.producer.ProgressControlInterceptor;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -9,13 +10,12 @@ import java.time.Duration;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-public class ControlProducerExample {
+public class ProgressControlInterceptorExample {
   public static void main(String[] args) throws ExecutionException, InterruptedException {
     final var config =
         Map.<String, Object>of(
             ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
-            ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, ProgressControlInterceptor.class.getName(),
-            ProducerConfig.ACKS_CONFIG, "1");
+            ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, ProgressControlInterceptor.class.getName());
     try (final var producer =
         new KafkaProducer<>(config, new StringSerializer(), new StringSerializer())) {
       producer.send(new ProducerRecord<>("input", "k1", "v1")).get();
