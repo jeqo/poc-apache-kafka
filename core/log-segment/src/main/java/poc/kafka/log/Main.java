d3535208037aea65463a25f375c041e938a31f11
Jorge Esteban Quilcate Otoya <jeqo@pm.me>
2023-04-09 11:27:37 +0300

    chore: organize alternatives

--- core/log-segment/src/main/java/poc/kafka/log/Main.java
+++ core/log-segment/src/main/java/poc/kafka/log/Main.java
@@ -1,6 +1,7 @@
 package poc.kafka.log;
 
 import kafka.log.OffsetIndex;
+import org.apache.kafka.common.record.CompressionType;
 import org.apache.kafka.common.record.FileRecords;
 import org.apache.kafka.common.record.RecordBatch;
 
@@ -12,21 +13,60 @@ import java.io.IOException;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        try (var records = FileRecords.open(new File("data/0PVBFBxoR2KSP8imYNgisQ/0PVBFBxoR2KSP8imYNgisQ.log"))) {
+        String prefix = "data/0PVBFBxoR2KSP8imYNgisQ";
+        File segmentFile = new File(prefix + "/0PVBFBxoR2KSP8imYNgisQ.log");
+        File offsetIndexFile = new File(prefix + "/0PVBFBxoR2KSP8imYNgisQ_offset.index");
+
+        try (var records = FileRecords.open(segmentFile)) {
             System.out.println(records.sizeInBytes());
             RecordBatch first = records.firstBatch();
             System.out.println("Compression at start: " + first.compressionType());
 
-            try (var offsetIndex = new OffsetIndex(new File("data/0PVBFBxoR2KSP8imYNgisQ/0PVBFBxoR2KSP8imYNgisQ_offset.index"), first.baseOffset(), -1, false)) {
-                System.out.println("Offset index " + offsetIndex.lastOffset());
+            {
+                // Alternative 1: naive iteration over batches
+                CompressionType lastCompressionType = null;
+                long lastBaseOffset = -1;
+                long lastLastOffset = -1;
+                int i = 0;
+                for (var r : records.batches()) {
+                    lastCompressionType = r.compressionType();
+                    lastBaseOffset = r.baseOffset();
+                    lastLastOffset = r.lastOffset();
+                    i++;
+                }
+                System.out.println("1: Compression at end: " + lastCompressionType
+                        + " from batch : [" + lastBaseOffset + ":" + lastLastOffset + "] batches: " + i);
+            }
 
-                var lastPos = offsetIndex.lookup(offsetIndex.lastOffset());
+            {
+                // Alternative 2: use offset index
+                try (var offsetIndex = new OffsetIndex(offsetIndexFile, first.baseOffset(), -1, false)) {
+                    System.out.println("2: Last offset from Index: " + offsetIndex.lastOffset());
 
-                System.out.println("Last position: " + lastPos.position());
-                var iter = records.batchesFrom(lastPos.position()).iterator();
-                if (iter.hasNext()) {
-                    var last = iter.next();
-                    System.out.println("Compression at end: " + last.compressionType());
+                    var lastPos = offsetIndex.lookup(offsetIndex.lastOffset());
+
+                    System.out.println("2: Last position: " + lastPos.position());
+
+                    // obtain the very last
+                    CompressionType lastCompressionType = null;
+                    long lastBaseOffset = -1;
+                    long lastLastOffset = -1;
+                    int i = 0;
+                    for (var r : records.batchesFrom(lastPos.position())) {
+                        lastCompressionType = r.compressionType();
+                        lastBaseOffset = r.baseOffset();
+                        lastLastOffset = r.lastOffset();
+                        i++;
+                    }
+                    System.out.println("2.1: Compression at end: " + lastCompressionType
+                            + " from batch : [" + lastBaseOffset + ":" + lastLastOffset + "] batches: " + i);
+
+                    // only from the first last batch (expecting only one, but could be more)
+                    var iter = records.batchesFrom(lastPos.position()).iterator();
+                    if (iter.hasNext()) {
+                        var last = iter.next();
+                        System.out.println("2.2: Compression at end: " + last.compressionType());
+                    }
                 }
             }
         }
